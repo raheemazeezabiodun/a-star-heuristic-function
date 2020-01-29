40c584c43b834a9125c621c615d766f2ec9cceac
Raheem Azeez Abiodun <raheemazeez4@gmail.com>
2020-01-29 15:59:45 +0100

    Heuristic function with Manhattan distance admissible function

--- searchHeuristic.cpp
+++ searchHeuristic.cpp
@@ -46,6 +46,13 @@ vector<vector<State>> ReadBoardFile(string path) {
   return board;
 }
 
+
+// Calculate the Manhattan distance
+int Heuristic(int x1, int y1, int x2, int y2) {
+  return abs(x2 - x1) + abs(y2 - y1);
+}
+
+
 vector<vector<State>> Search (vector<vector<State>> grid, int init[2], int goal[2]) {
   cout << "No path found! \n";
 
